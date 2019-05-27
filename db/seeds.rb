5bc17017905ec97f93da231c2291d64e1c855390
Tim Moon <tim.moon7@gmail.com>
2019-05-28 00:45:21 +1000

    Add Genres seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Genre.create!([ 
+    { name: "Adventure" },
+    { name: "Drama" },
+    { name: "Action" },
+    { name: "Comedy" },
+    { name: "Biography" },
+    { name: "Fantasy" },
+    { name: "War" }
+])
