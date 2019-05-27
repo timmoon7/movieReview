ff07b1be69f6285461194f8b5153ac4f082d1a16
Tim Moon <tim.moon7@gmail.com>
2019-05-28 00:06:39 +1000

    Change Database from Sqlite3 to Postgresql

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'jquery-rails'
 gem 'devise', '~> 4.6', '>= 4.6.2'
 gem 'paperclip', '~> 6.1'
+gem 'pg', '~> 1.1', '>= 1.1.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_26_112521) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
