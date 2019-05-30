9b1660ced602f4bf7ec2ae7d0369259b3d7edce7
Tim Moon <tim.moon7@gmail.com>
2019-05-30 20:09:47 +1000

    Add default movie image

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ApplicationRecord
   belongs_to :genre
   has_many :reviews
 
-  has_attached_file :movie_img, styles: { movie_index: "250x350>", movie_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :movie_img, styles: { movie_index: "250x350>", movie_show: "325x475>" }, default_url: "missing.png"
   validates_attachment_content_type :movie_img, content_type: /\Aimage\/.*\z/
 end
